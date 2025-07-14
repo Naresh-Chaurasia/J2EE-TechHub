cb897f5e07e95c7f77bbea627f036b3f1b06e90f
Naresh Chaurasia <johndoe@example.com>
2025-07-14 09:09:54 +0530

    first commit

---
+++ spring-boot-application/src/main/java/com/tutorials/Application.java
@@ -0,0 +1,14 @@
+package com.tutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		// System.out.println("Starting the Spring Boot application...");
+		SpringApplication.run(Application.class, args);
+	}
+
+}
