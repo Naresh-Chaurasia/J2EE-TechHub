b4bfc116c14bcbf4fda2840a91f676154c7bc691
Naresh Chaurasia <johndoe@example.com>
2025-07-14 09:48:58 +0530

    Macbook Pro, Checkin Timestamp::Mon Jul 14 09:48:58 IST 2025

--- spring-boot-application/src/main/java/com/tutorials/repository/UserRepository.java
+++ spring-boot-application/src/main/java/com/tutorials/repository/UserRepository.java
@@ -6,4 +6,19 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    // save(S entity)
+    // saveAll(Iterable<S> entities)
+    // findById(ID id)
+    // existsById(ID id)
+    // findAll()
+    // findAllById(Iterable<ID> ids)
+    // count()
+    // deleteById(ID id)
+    // delete(T entity)
+    // deleteAllById(Iterable<? extends ID> ids)
+    // deleteAll(Iterable<? extends T> entities)
+    // deleteAll()
+    // findAll(Sort sort)
+    // findAll(Pageable pageable)
 }
